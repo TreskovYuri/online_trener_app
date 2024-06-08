174a5efb8d9af8b5082429c760d24a9e6c3396e1
TreskovYuri <169077900+TreskovYuri@users.noreply.github.com>
2024-06-08 17:51:03 +0300

    wedfwe

--- lib/pages/trener/sevice/faq/faq.dart
+++ lib/pages/trener/sevice/faq/faq.dart
@@ -11,9 +11,7 @@ class Faq extends StatefulWidget {
 }
 
 class _FaqState extends State<Faq> {
-
-
-    ScrollController _scrollController = ScrollController();
+  ScrollController _scrollController = ScrollController();
   bool _isAtTop = true;
 
   @override
@@ -33,7 +31,8 @@ class _FaqState extends State<Faq> {
       _isAtTop = _scrollController.position.pixels == 0;
     });
   }
-@override
+
+  @override
   Widget build(BuildContext context) {
     final vw = MediaQuery.of(context).size.width / 100;
     final vh = MediaQuery.of(context).size.height / 100;
@@ -44,10 +43,10 @@ class _FaqState extends State<Faq> {
           icon: const Icon(Icons
               .arrow_back_ios_new_rounded), // Устанавливаем иконку "домой" вместо стрелки "назад"
           onPressed: () {
-            Navigator.pushReplacementNamed(context,'/service');
+            Navigator.pushReplacementNamed(context, '/service');
           },
         ),
-        toolbarHeight: 13*vh,
+        toolbarHeight: 13 * vh,
         title: const Text(
           'Справочник',
           style: TextStyle(
@@ -58,208 +57,222 @@ class _FaqState extends State<Faq> {
         centerTitle: true,
         backgroundColor: Color(0xff1B1C20),
       ),
-      body: Stack(
-        fit: StackFit.expand,
-        children: [
-          Container(
-            padding: EdgeInsets.all(3 * vw),
-            child: Column(
-              children: [
-                SizedBox(height: 4*vh,),
-                ElevatedButton(
-                  style: ElevatedButton.styleFrom(
-                    padding:
-                        EdgeInsets.symmetric(vertical: 2 * vh, horizontal: 5 * vw),
-                    backgroundColor: Color(0xff23252B),
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(
-                          8.0), // Указываем радиус скругления углов
+      body: SizedBox(
+        height: 79 * vh,
+        child: Stack(
+          fit: StackFit.expand,
+          children: [
+            Container(
+              padding: EdgeInsets.all(3 * vw),
+              child: Column(
+                children: [
+                  SizedBox(
+                    height: 4 * vh,
+                  ),
+                  ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      padding: EdgeInsets.symmetric(
+                          vertical: 2 * vh, horizontal: 5 * vw),
+                      backgroundColor: Color(0xff23252B),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(
+                            8.0), // Указываем радиус скругления углов
+                      ),
+                    ),
+                    onPressed: () {
+                      Navigator.pushReplacementNamed(
+                          context, '/trener_trainings');
+                    },
+                    child: Container(
+                      width: 90 * vw,
+                      alignment: Alignment.centerLeft,
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: [
+                          Row(
+                            children: [
+                              SvgPicture.asset(
+                                'assets/img/pleers_trener.svg',
+                                width: 4.4 * vw,
+                              ),
+                              SizedBox(
+                                width: 3 * vw,
+                              ),
+                              Text(
+                                'Упражнения',
+                                style: TextStyle(
+                                    color: Colors.white,
+                                    fontWeight: FontWeight.w400,
+                                    fontFamily: "Manrope",
+                                    fontSize: 4.5 * vw),
+                              ),
+                            ],
+                          ),
+                          Icon(
+                            Icons.arrow_forward_ios_rounded,
+                            color: Colors.white,
+                            size: 4 * vw,
+                          )
+                        ],
+                      ),
                     ),
                   ),
-                  onPressed: () {
-                    Navigator.pushReplacementNamed(context, '/trener_trainings');
-                  },
-                  child: Container(
-                    width: 90 * vw,
-                    alignment: Alignment.centerLeft,
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      children: [
-                        Row(
-                          children: [
-                            SvgPicture.asset(
-                              'assets/img/pleers_trener.svg',
-                              width: 4.4 * vw,
-                            ),
-                            SizedBox(
-                              width: 3 * vw,
-                            ),
-                            Text(
-                              'Упражнения',
-                              style: TextStyle(
-                                  color: Colors.white,
-                                  fontWeight: FontWeight.w400,
-                                  fontFamily: "Manrope",
-                                  fontSize: 4.5 * vw),
-                            ),
-                          ],
-                        ),
-                        Icon(
-                          Icons.arrow_forward_ios_rounded,
-                          color: Colors.white,
-                          size: 4 * vw,
-                        )
-                      ],
+                  SizedBox(
+                    height: 2 * vh,
+                  ),
+                  ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      padding: EdgeInsets.symmetric(
+                          vertical: 2 * vh, horizontal: 5 * vw),
+                      backgroundColor: Color(0xff23252B),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(
+                            8.0), // Указываем радиус скругления углов
+                      ),
+                    ),
+                    onPressed: () {
+                      Navigator.pushReplacementNamed(context, '/trener_tests');
+                    },
+                    child: Container(
+                      width: 90 * vw,
+                      alignment: Alignment.centerLeft,
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: [
+                          Row(
+                            children: [
+                              SvgPicture.asset(
+                                'assets/img/tests.svg',
+                                width: 4.4 * vw,
+                              ),
+                              SizedBox(
+                                width: 3 * vw,
+                              ),
+                              Text(
+                                'Тесты',
+                                style: TextStyle(
+                                    color: Colors.white,
+                                    fontWeight: FontWeight.w400,
+                                    fontFamily: "Manrope",
+                                    fontSize: 4.5 * vw),
+                              ),
+                            ],
+                          ),
+                          Icon(
+                            Icons.arrow_forward_ios_rounded,
+                            color: Colors.white,
+                            size: 4 * vw,
+                          )
+                        ],
+                      ),
                     ),
                   ),
-                ),
-                SizedBox(height: 2*vh,),
-                ElevatedButton(
-                  style: ElevatedButton.styleFrom(
-                    padding:
-                        EdgeInsets.symmetric(vertical: 2 * vh, horizontal: 5 * vw),
-                    backgroundColor: Color(0xff23252B),
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(
-                          8.0), // Указываем радиус скругления углов
+                  SizedBox(
+                    height: 2 * vh,
+                  ),
+                  ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      padding: EdgeInsets.symmetric(
+                          vertical: 2 * vh, horizontal: 5 * vw),
+                      backgroundColor: Color(0xff23252B),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(
+                            8.0), // Указываем радиус скругления углов
+                      ),
+                    ),
+                    onPressed: () {},
+                    child: Container(
+                      width: 90 * vw,
+                      alignment: Alignment.centerLeft,
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: [
+                          Row(
+                            children: [
+                              SvgPicture.asset(
+                                'assets/img/zdorovie.svg',
+                                width: 4.4 * vw,
+                              ),
+                              SizedBox(
+                                width: 3 * vw,
+                              ),
+                              Text(
+                                'Показатели здоровья',
+                                style: TextStyle(
+                                    color: Colors.white,
+                                    fontWeight: FontWeight.w400,
+                                    fontFamily: "Manrope",
+                                    fontSize: 4.5 * vw),
+                              ),
+                            ],
+                          ),
+                          Icon(
+                            Icons.arrow_forward_ios_rounded,
+                            color: Colors.white,
+                            size: 4 * vw,
+                          )
+                        ],
+                      ),
                     ),
                   ),
-                  onPressed: () {Navigator.pushReplacementNamed(context, '/trener_tests');},
-                  child: Container(
-                    width: 90 * vw,
-                    alignment: Alignment.centerLeft,
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      children: [
-                        Row(
-                          children: [
-                            SvgPicture.asset(
-                              'assets/img/tests.svg',
-                              width: 4.4 * vw,
-                            ),
-                            SizedBox(
-                              width: 3 * vw,
-                            ),
-                            Text(
-                              'Тесты',
-                              style: TextStyle(
-                                  color: Colors.white,
-                                  fontWeight: FontWeight.w400,
-                                  fontFamily: "Manrope",
-                                  fontSize: 4.5 * vw),
-                            ),
-                          ],
-                        ),
-                        Icon(
-                          Icons.arrow_forward_ios_rounded,
-                          color: Colors.white,
-                          size: 4 * vw,
-                        )
-                      ],
+                  SizedBox(
+                    height: 2 * vh,
+                  ),
+                  ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      padding: EdgeInsets.symmetric(
+                          vertical: 2 * vh, horizontal: 5 * vw),
+                      backgroundColor: Color(0xff23252B),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(
+                            8.0), // Указываем радиус скругления углов
+                      ),
+                    ),
+                    onPressed: () {},
+                    child: Container(
+                      width: 90 * vw,
+                      alignment: Alignment.centerLeft,
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: [
+                          Row(
+                            children: [
+                              SvgPicture.asset(
+                                'assets/img/med.svg',
+                                width: 4.4 * vw,
+                              ),
+                              SizedBox(
+                                width: 3 * vw,
+                              ),
+                              Text(
+                                'Медицинские анализы',
+                                style: TextStyle(
+                                    color: Colors.white,
+                                    fontWeight: FontWeight.w400,
+                                    fontFamily: "Manrope",
+                                    fontSize: 4.5 * vw),
+                              ),
+                            ],
+                          ),
+                          Icon(
+                            Icons.arrow_forward_ios_rounded,
+                            color: Colors.white,
+                            size: 4 * vw,
+                          )
+                        ],
+                      ),
                     ),
                   ),
-                ),
-                SizedBox(height: 2*vh,),
-                ElevatedButton(
-                  style: ElevatedButton.styleFrom(
-                    padding:
-                        EdgeInsets.symmetric(vertical: 2 * vh, horizontal: 5 * vw),
-                    backgroundColor: Color(0xff23252B),
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(
-                          8.0), // Указываем радиус скругления углов
-                    ),
-                  ),
-                  onPressed: () {},
-                  child: Container(
-                    width: 90 * vw,
-                    alignment: Alignment.centerLeft,
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      children: [
-                        Row(
-                          children: [
-                            SvgPicture.asset(
-                              'assets/img/zdorovie.svg',
-                              width: 4.4 * vw,
-                            ),
-                            SizedBox(
-                              width: 3 * vw,
-                            ),
-                            Text(
-                              'Показатели здоровья',
-                              style: TextStyle(
-                                  color: Colors.white,
-                                  fontWeight: FontWeight.w400,
-                                  fontFamily: "Manrope",
-                                  fontSize: 4.5 * vw),
-                            ),
-                          ],
-                        ),
-                        Icon(
-                          Icons.arrow_forward_ios_rounded,
-                          color: Colors.white,
-                          size: 4 * vw,
-                        )
-                      ],
-                    ),
-                  ),
-                ),
-                SizedBox(height: 2*vh,),
-                ElevatedButton(
-                  style: ElevatedButton.styleFrom(
-                    padding:
-                        EdgeInsets.symmetric(vertical: 2 * vh, horizontal: 5 * vw),
-                    backgroundColor: Color(0xff23252B),
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(
-                          8.0), // Указываем радиус скругления углов
-                    ),
-                  ),
-                  onPressed: () {},
-                  child: Container(
-                    width: 90 * vw,
-                    alignment: Alignment.centerLeft,
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      children: [
-                        Row(
-                          children: [
-                            SvgPicture.asset(
-                              'assets/img/med.svg',
-                              width: 4.4 * vw,
-                            ),
-                            SizedBox(
-                              width: 3 * vw,
-                            ),
-                            Text(
-                              'Медицинские анализы',
-                              style: TextStyle(
-                                  color: Colors.white,
-                                  fontWeight: FontWeight.w400,
-                                  fontFamily: "Manrope",
-                                  fontSize: 4.5 * vw),
-                            ),
-                          ],
-                        ),
-                        Icon(
-                          Icons.arrow_forward_ios_rounded,
-                          color: Colors.white,
-                          size: 4 * vw,
-                        )
-                      ],
-                    ),
-                  ),
-                ),
-              ],
+                ],
+              ),
             ),
-          ),
-          _isAtTop ? Navbar() : NavbarScroll(),
-        ],
+            _isAtTop ? Navbar() : NavbarScroll(),
+          ],
+        ),
       ),
     );
   }
